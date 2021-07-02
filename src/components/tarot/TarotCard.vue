352c4d458d8d65791a3778bc09f8d86fdf10e56e
jraynolds <jaspirian@gmail.com>
2021-07-02 03:45:10 -0700

    New tarot generation added.

--- src/components/tarot/TarotCard.vue
+++ src/components/tarot/TarotCard.vue
@@ -20,7 +20,7 @@
 						class="d-flex justify-center fill-height"
 					>
 						<v-card-title>
-							{{ cardBackTitle }}
+							{{ cardBack }}
 						</v-card-title>
 					</v-col>
 				</v-card>
@@ -38,12 +38,16 @@
 						class="fill-height d-flex flex-column"
 						:style="{backgroundColor: backColor}"
 					>
-						<v-row class="justify-center">
+						<v-row 
+							class="justify-center" 
+							:style="isReversed ? { transform: 'rotate(180deg)' } : {}"
+						>
 							<v-col 
 								cols="4" 
 								v-for="i in iconNumber" 
 								:key="i"
 								class="d-flex align-middle justify-center"
+								:style="isMajorArcana ? { transform: 'scale(3)' } : {}"
 							>
 								<v-icon x-large>
 									{{ icon }}
@@ -71,14 +75,19 @@ import VueFlip from "vue-flip"
 export default {
 	props: [
 		"card",
-		"arcana"
+		"arcana",
+		"cardBack",
+		"prepend",
+		"append",
+		"reversible",
+		"small"
 	],
 	components: {
 		VueFlip
 	},
 	data: () => ({
-		height: 500,
-		width: 350,
+		heights: [ 500, 450 ],
+		widths: [ 350, 300 ],
 		edgeColor: "silver",
 		backColor: "#152736",
 		textBackColor: "#0e1a24",
@@ -103,31 +112,78 @@ export default {
 			cups: "mdi-glass-cocktail",
 			pentacles: "mdi-star-circle",
 			wands: "mdi-pencil",
-		}
+		},
+		majorToIcon: {
+			"The Fool": "mdi-emoticon-excited",
+			"The Magician": "mdi-auto-fix",
+			"The High Priestess": "mdi-snake",
+			"The Empress": "mdi-mother-heart",
+			"The Emperor": "mdi-human-male",
+			"The Hierophant": "mdi-book-cross",
+			"The Lovers": "mdi-human-male-female",
+			"The Chariot": "mdi-horse-human",
+			"Strength": "mdi-weight-lifter",
+			"The Hermit": "mdi-school",
+			"Wheel of Fortune": "mdi-ship-wheel",
+			"Justice": "mdi-scale-balance",
+			"The Hanged Man": "mdi-lasso",
+			"Death": "mdi-skull",
+			"Temperance": "mdi-yin-yang",
+			"The Devil": "mdi-emoticon-devil",
+			"The Tower": "mdi-chess-rook",
+			"The Star": "mdi-star-shooting",
+			"The Moon": "mdi-weather-night",
+			"The Sun": "mdi-white-balance-sunny",
+			"Judgment": "mdi-gavel",
+			"The World": "mdi-earth"
+		},
+		random: 0
 	}),
+	watch: {
+		card() {
+			this.random = Math.random();
+		}
+	},
 	computed: {
-		cardBackTitle() {
-			if (this.arcana == "major") return "ARCHETYPE";
-			return "PATTERN";
+		isMajorArcana() {
+			return this.card.title;
 		},
 		title() {
-			if (this.card.title) return this.card.title;
+			let s = this.card.title;
+			if (!s) s = `${this.numberToWord[this.card.number]} of ${this.card.suit}`;
+			if (this.isReversed) s += " (Reversed)"
 
-			let word = this.numberToWord[this.card.number];
-			return `${word} of ${this.card.suit}`
+			return s;
+		},
+		height() {
+			if (this.small) return this.heights[1];
+			return this.heights[0];
+		},
+		width() {
+			if (this.small) return this.widths[1];
+			return this.widths[0];
+		},
+		isReversed() {
+			if (!this.reversible) return false;
+			return this.random >= .5;
 		},
 		description() {
-			if (this.arcana == "major") return this.card.description + "...";
-			return "..." + this.card.description;
+			let s = this.card.description;
+			if (!s && !this.isReversed) s = this.card.temperaments.join(", ");
+			if (!s && this.isReversed) s = this.card.reversedTemperaments.join(", ");
+
+			if (this.prepend) s = this.prepend + s;
+			if (this.append) s = s + this.append;
+			
+			return s;
 		},
 		icon() {
-			if (this.card.icon) return this.card.icon;
-
+			if (this.card.title) return this.majorToIcon[this.card.title];
 			return this.suitToIcon[this.card.suit];
 		},
 		iconNumber() {
-			if (this.arcana == "minor") return this.card.number;
-			return 1; 
+			if (this.card.title) return 1; 
+			return this.card.number;
 		}
 	}
 }
--- src/views/Tarot.vue
+++ src/views/Tarot.vue
@@ -1,32 +1,94 @@
 <template>
 	<v-container class="fill-height d-flex justify-space-between">
-		<v-row class="text-center">
-			<v-col cols="6" class="d-flex justify-center align-center">
-				<TarotCard 
-					:card="major" 
-					arcana="major" 
-				/>
-			</v-col>
-			<v-col cols="6">
-				<TarotCard 
-					:card="minor" 
-					arcana="minor" 
-				/>
-			</v-col>
+		<v-row class="text-center align-center justify-center">
+			Archetype/Pattern
+			<v-switch style="transform: scale(1.5);" v-model="mode" class="mx-8" />
+			Archetype/Personality/Cycle
 		</v-row>
-		<v-row class="justify-center" >
-			<v-col cols="8">
-				<v-btn 
-					color="primary" 
-					large 
-					style="font-size: large; width: 100%;"
-					@click="redraw()"
-				>
-					<v-icon large left>mdi-refresh</v-icon>
-					Refresh
-				</v-btn>
-			</v-col>
-		</v-row>
+
+		<v-col v-if="!mode">
+			<v-row class="text-center">
+				<v-col cols="6" class="d-flex justify-center align-center">
+					<TarotCard 
+						:card="archetype" 
+						arcana="major"
+						cardBack="ARCHETYPE"
+						append="..."
+					/>
+				</v-col>
+				<v-col cols="6">
+					<TarotCard 
+						:card="pattern" 
+						arcana="minor"
+						cardBack="PATTERN"
+						prepend="..."
+						append="."
+					/>
+				</v-col>
+			</v-row>
+			<v-row class="justify-center" >
+				<v-col cols="8">
+					<v-btn 
+						color="primary" 
+						large 
+						style="font-size: large; width: 100%;"
+						@click="redraw()"
+					>
+						<v-icon large left>mdi-refresh</v-icon>
+						Refresh
+					</v-btn>
+				</v-col>
+			</v-row>
+		</v-col>
+
+		<v-col v-else>
+			<v-row class="text-center">
+				<v-col cols="4" class="d-flex justify-center align-center">
+					<TarotCard 
+						:card="archetype" 
+						arcana="major"
+						cardBack="ARCHETYPE"
+						append="..."
+						:small="true"
+					/>
+				</v-col>
+				<v-col cols="4">
+					<TarotCard 
+						:card="personality" 
+						arcana="all"
+						cardBack="PERSONALITY"
+						prepend="..."
+						append="..."
+						:reversible="true"
+						:small="true"
+					/>
+				</v-col>
+				<v-col cols="4">
+					<TarotCard 
+						:card="cycle" 
+						arcana="all"
+						cardBack="CYCLE"
+						prepend="...Forever "
+						append="."
+						:small="true"
+					/>
+				</v-col>
+			</v-row>
+			<v-row class="justify-center" >
+				<v-col cols="8">
+					<v-btn 
+						color="primary" 
+						large 
+						style="font-size: large; width: 100%;"
+						@click="redraw()"
+					>
+						<v-icon large left>mdi-refresh</v-icon>
+						Refresh
+					</v-btn>
+				</v-col>
+			</v-row>
+		</v-col>
+
 	</v-container>
 </template>
 
@@ -39,8 +101,13 @@ export default {
 	},
 	data: () => ({
 		cards: require("@/assets/tarot/cards.json"),
-		major: null,
-		minor: null
+		archetype: null,
+		pattern: null,
+
+		personality: null,
+		cycle: null,
+
+		mode: false
 	}),
 	methods: {
 		getRandomCard(arcana) {
@@ -49,8 +116,12 @@ export default {
 			]
 		},
 		redraw() {
-			this.minor = this.getRandomCard("minor");
-			this.major = this.getRandomCard("major");
+			this.pattern = this.getRandomCard("minorPersonality");
+
+			this.archetype = this.getRandomCard("majorArchetype");
+
+			this.personality = this.getRandomCard("allPersonality");
+			this.cycle = this.getRandomCard("allCycle");
 		}
 	},
 	beforeMount() {
